5ecbbb4b2809f925bb9aab622ca3f80276a814b7
Brandon Griffin <bgriffin@qualtrax.com>
2014-01-06 15:02:45 -0500

    Refactored BowlingGameScorer a little.

--- BowlingGameKata/BowlingGameScorer.cs
+++ BowlingGameKata/BowlingGameScorer.cs
@@ -8,9 +8,9 @@
     
     public class BowlingGameScorer
     {
-        
         private int[] scores = new int[21];
         private int numberOfRolls = 0;
+
         public void Roll(int pins)
         {
             scores[numberOfRolls] = pins;
@@ -24,28 +24,54 @@
 
             for (int frame = 0; frame < 10; frame++)
 			{
-                if (scores[roll] == 10)
+                if (IsAStrike(roll))
                 {
-                    score += 10;
-                    score += scores[roll + 1];
-                    score += scores[roll + 2];
+                    score = AddScoreForStrike(score, roll);
                     roll += 1;
                 }
-
-                else if ((scores[roll] + scores[roll + 1]) == 10)
+                else if (IsASpare(roll))
                 {
-                    score += 10;
-                    score += scores[roll + 2];
+                    score = AddScoreForSpare(score, roll);
                     roll += 2;
                 }
                 else
                 {
-                    score += scores[roll] + scores[roll + 1];
+                    score += AddScoreForFrame(roll);
                     roll += 2;
                 }
 			}
 
             return score;
+        }
+
+        private int AddScoreForFrame(int roll)
+        {
+            return scores[roll] + scores[roll + 1];
+        }
+
+        private int AddScoreForSpare(int score, int roll)
+        {
+            score += 10;
+            score += scores[roll + 2];
+            return score;
+        }
+
+        private int AddScoreForStrike(int score, int roll)
+        {
+            score += 10;
+            score += scores[roll + 1];
+            score += scores[roll + 2];
+            return score;
+        }
+
+        private bool IsASpare(int roll)
+        {
+            return (scores[roll] + scores[roll + 1]) == 10;
+        }
+
+        private bool IsAStrike(int roll)
+        {
+            return scores[roll] == 10;
         }
     }
 }
