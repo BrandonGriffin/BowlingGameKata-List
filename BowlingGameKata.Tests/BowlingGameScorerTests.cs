e1cb04772f9ac72b5b29ac07f800405cd40c5fe5
Brandon Griffin <bgriffin@qualtrax.com>
2014-01-06 16:48:15 -0500

    Changed privates from _name to name.

--- BowlingGameKata.Tests/BowlingGameScorerTests.cs
+++ BowlingGameKata.Tests/BowlingGameScorerTests.cs
@@ -10,19 +10,19 @@
     [TestFixture]
     public class BowlingGameScorerTests
     {
-        private BowlingGameScorer _scorer;
+        private BowlingGameScorer scorer;
 
         [SetUp]
         public void SetUp()
         {
-            _scorer = new BowlingGameScorer();
+            scorer = new BowlingGameScorer();
         }
 
         [Test]
         public void AGutterGameShouldReturn0()
         { 
             RollMany(0, 20);
-            var actual = _scorer.GetScore();
+            var actual = scorer.GetScore();
 
             Assert.That(actual, Is.EqualTo(0));
         }
@@ -31,7 +31,7 @@
         public void AGameOfOnesShouldScore20()
         {
             RollMany(1, 20);
-            var actual = _scorer.GetScore();
+            var actual = scorer.GetScore();
             
             Assert.That(actual, Is.EqualTo(20));
         }
@@ -39,11 +39,11 @@
         [Test]
         public void ASpareShouldCountTheNextRollTwice()
         {
-            _scorer.Roll(4);
-            _scorer.Roll(6);
-            _scorer.Roll(3);
+            scorer.Roll(4);
+            scorer.Roll(6);
+            scorer.Roll(3);
             RollMany(0, 17);
-            var actual = _scorer.GetScore();
+            var actual = scorer.GetScore();
 
             Assert.That(actual, Is.EqualTo(16));
         }
@@ -51,11 +51,11 @@
         [Test]
         public void AStrikeShouldCountTheNextTwoRolls()
         {
-            _scorer.Roll(10);
-            _scorer.Roll(4);
-            _scorer.Roll(3);
+            scorer.Roll(10);
+            scorer.Roll(4);
+            scorer.Roll(3);
             RollMany(0, 17);
-            var actual = _scorer.GetScore();
+            var actual = scorer.GetScore();
 
             Assert.That(actual, Is.EqualTo(24));
         }
@@ -64,16 +64,27 @@
         public void APerfectGameShouldGive300()
         {
             RollMany(10, 12);
-            var actual = _scorer.GetScore();
+            var actual = scorer.GetScore();
 
             Assert.That(actual, Is.EqualTo(300));
+        }
+
+        [Test]
+        public void ARandomGameShouldScore80()
+        {
+            RollMany(4, 8);
+            RollMany(6, 4);
+            RollMany(3, 8);
+            var actual = scorer.GetScore();
+
+            Assert.That(actual, Is.EqualTo(80));
         }
 
         private void RollMany(int pins, int timesToRoll)
         {
             for (int i = 0; i < timesToRoll; i++)
             {
-                _scorer.Roll(pins);
+                scorer.Roll(pins);
             }
         }
     }
